9e5af2022194e8ccfa00f6f662253d878813fa07
Ogundare Taiwo <Chrissito55@gmail.com>
2023-02-07 15:55:24 +0100

    Add project files.

---
+++ Calculator/Controllers/OperatorsController1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Calculator.Models;
+
+namespace Calculator.Controllers
+{
+    public class OperatorsController1 : Controller
+    {
+        public IActionResult Index()
+
+        {
+            return View();
+        }
+    }
+}
---
+++ Calculator/Models/Operators.cs
@@ -0,0 +1,12 @@
+﻿namespace Calculator.Models
+{
+    public class Operators
+    {
+        public string Multiply = "*";
+        public string Add = "+";
+        public string Subtract = "-";
+        public string Modulo = "%";
+        public string Divide = "/";
+        public Operators() { }
+    }
+}
